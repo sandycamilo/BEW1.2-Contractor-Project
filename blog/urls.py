544970bcdb8630715ec03b524c7147e371e26e03
sandycamilo <scam499@lasierra.edu>
2020-03-02 04:25:28 -0800

    blog post form

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostlistView, PostDetailView
+from .views import PostlistView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
     path('', PostlistView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
 
