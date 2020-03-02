6c96dfbb183b5b30caac98ed19ac660cd4e2655c
sandycamilo <scam499@lasierra.edu>
2020-03-01 22:36:54 -0800

    base.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'SandyC',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Today'
+    },
+    {
+        'author': 'molly',
+        'title': 'Blog Post 2',
+        'content': 'second post content',
+        'date_posted': 'yesterday'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
