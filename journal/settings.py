b1509d82a553ba3cfb2ba61989b8db247d087d3b
sandycamilo <scam499@lasierra.edu>
2020-03-02 03:09:18 -0800

    default img

--- journal/settings.py
+++ journal/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = ''
-MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
