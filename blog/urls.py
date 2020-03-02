77e85c18b94663b9ee024ddabadf9c379be21290
sandycamilo <scam499@lasierra.edu>
2020-03-02 13:11:26 -0800

    user posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostlistView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostlistView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostlistView
 from . import views
 
 urlpatterns = [
     path('', PostlistView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostlistView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
