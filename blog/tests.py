9374199ea3097c9b4d430a898c5baf2d46e72aa2
sandycamilo <scam499@lasierra.edu>
2020-03-06 07:40:27 -0800

    tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from blog.models import Post
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+    def test_true_is_true(self):
+        """ Tests is Trues is equal to True. Should always pass. """ 
+        self.assertEqual(True, True)
+
+    def test_page_slugify_on_save(self):
+        """ Tests the slug generated when saving a Post. """
+        user = User()
+        user.save()
+
+        post = Post(title="My Test Page", content="test", author=user)
+        post.save()
+
+        self.assertEqual(post.slug, "my-test-page")
+
+class PostListViewTests(TestCase):
+    def test_multiple_posts(self):
+        user = User.objects.create()
+
+        Post.objects.create(title="My Test Post", content="test", author=user)
+        Post.objects.create(title="Other", content="test", author=user)
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        responses = response.context['pages']
+        self.assertEqual(len(responses), 2)
+
+        self.assertQueryEqual(
+            responses, 
+            ['<Post: My Test Page', '<Post:Other'],
+            ordered=False
+        )
+
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client, SimpleTestCase
+from django.urls import reverse
+from users.forms import UserRegisterForm, UserUpdateForm
 
-# Create your tests here.
+class TestForms_Registration(TestCase):
+    def test_registration_form_with_data(self):
+        form = UserRegisterForm(data={
+            'username':'UserTest',
+            'email':'userTest@email.com',
+            'password1':'1234redbottle',
+            'password2':'1234redbottle'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_registration_form_no_data(self):
+        form = UserRegisterForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors), 4)
+
+class TestForms_UserUpdate(TestCase):
+    def test_userupdate_form_no_data(self):
+        form = UserUpdateForm(data={
+            'username':'userTest',
+            'email':'userTest@email.com'
+        })
+        self.assertTrue(form.is_valid())
+        self.assertEquals(len(form.errors), 0)
