6bfc5366e395fede4b883f553ba1d39c7e9a946d
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-05-06 23:19:00 -0700

    captain def

---
+++ py3env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sandyscamilo/MakeSchool/BEW1.2/Contractor_Project/journal/py3env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
