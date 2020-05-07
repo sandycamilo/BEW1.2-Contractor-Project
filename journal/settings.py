cc289fecdebac3098956637bd9bc00b7e17634c1
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-05-06 23:46:40 -0700

    h

--- journal/settings.py
+++ journal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from os.path import join, dirname
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
